be636ac1bd4502ce4317c15408462f8540c10fcf
ArmannGr <grewal.armann15@gmail.com>
2025-02-24 20:39:22 +0100

    feat(auth): add login, register and logout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {AuthService} from '@auth0/auth0-angular';
+import {RouterOutlet} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Frontend';
+
+  ngOnInit() {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (!isAuthenticated) {
+        this.auth.loginWithRedirect()
+      }
+    });
+  }
+
+  constructor( public auth: AuthService) {}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideAuth0} from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'dev-4u2jxny8glnhls6d.eu.auth0.com',
+      clientId: 'Vto5pBhpugSi309XIvWtGfh6mlWlEitZ',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+  ]
 };
