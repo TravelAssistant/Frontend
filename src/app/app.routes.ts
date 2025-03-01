8d74a682d742c1a3041e2105ea5fa74c1f272109
Mauritz Langer <mauritz@langer-net.de>
2025-03-01 11:11:36 +0100

    feat(map): adding map page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {ProfileComponent} from './components/profile/profile.component';
+import {MapPageComponent} from './components/map-page/map-page.component';
 
 export const routes: Routes = [
   {
     component: ProfileComponent,
     path: '',
+  },
+  {
+    component: MapPageComponent,
+    path: 'map',
   }
 ];
