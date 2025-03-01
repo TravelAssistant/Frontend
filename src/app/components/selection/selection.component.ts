a91c81c9aea4aec69f7c4e2368a5ec1f39229e74
alexandereisner <a.eisner.1822@gmail.com>
2025-03-01 12:14:01 +0100

    feat(frontend): fixed selection and added output of inputs

--- src/app/components/selection/selection.component.ts
+++ src/app/components/selection/selection.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatOption, MatSelect} from '@angular/material/select';
 import {MatButton} from '@angular/material/button';
 import {MatInput} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-selection',
@@ -12,11 +13,23 @@ import {MatInput} from '@angular/material/input';
     MatOption,
     MatButton,
     MatInput,
-    MatLabel
+    MatLabel,
+    FormsModule
   ],
   templateUrl: './selection.component.html',
   styleUrl: './selection.component.css'
 })
 export class SelectionComponent {
+
+  input1: string = '';
+  input2: string = '';
+
+  dropdownOptions: string[] = ['Auto', 'Flugzeug', 'Zug'];
+  selectedOption: string = this.dropdownOptions[0];
+
+  showAlert(): void {
+    alert(`Eingabe 1: ${this.input1}\nEingabe 2: ${this.input2}\nAuswahl: ${this.selectedOption}`);
+  }
+
 
 }
