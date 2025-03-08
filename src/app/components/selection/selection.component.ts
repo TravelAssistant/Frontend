6f704380e2318f163d43654ca5dd5a32aa1812ce
alexandereisner <a.eisner.1822@gmail.com>
2025-03-08 12:31:14 +0100

    feat(frontend): add lambda api consumption with dummy data

--- src/app/components/selection/selection.component.ts
+++ src/app/components/selection/selection.component.ts
@@ -4,6 +4,7 @@ import {MatOption, MatSelect} from '@angular/material/select';
 import {MatButton} from '@angular/material/button';
 import {MatInput} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-selection',
@@ -14,22 +15,47 @@ import {FormsModule} from '@angular/forms';
     MatButton,
     MatInput,
     MatLabel,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   templateUrl: './selection.component.html',
   styleUrl: './selection.component.css'
 })
 export class SelectionComponent {
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+
   input1: string = '';
   input2: string = '';
+  routeData: any;
 
   dropdownOptions: string[] = ['Auto', 'Flugzeug', 'Zug'];
   selectedOption: string = this.dropdownOptions[0];
 
-  showAlert(): void {
-    alert(`Eingabe 1: ${this.input1}\nEingabe 2: ${this.input2}\nAuswahl: ${this.selectedOption}`);
+  getRouteData(): void {
+    const apiUrl = 'https://164qy48jr1.execute-api.eu-central-1.amazonaws.com/prod/';
+    const params = { Start: this.input1, Ziel: this.input2 };
+
+    this.http.get(apiUrl, { params })
+      .subscribe(response => {
+        this.routeData = response;
+        this.showAlert();
+      });
   }
+
+  showAlert(): void {
+    if (this.routeData) {
+      const message = `Strecke: ${this.routeData.Strecke}, CO2: ${this.routeData.CO2}, Preis: ${this.routeData.Preis}`;
+      alert(message);
+    } else {
+      alert('Keine Daten gefunden');
+    }
+  }
+
 
 
 }
